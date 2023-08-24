853867e96ec09bc0255560db5261b955a764d05f
BienBoy <285915362@qq.com>
2023-08-24 13:57:03 +0800

    配置文件使用环境变量

--- summer_backend/urls.py
+++ summer_backend/urls.py
@@ -18,5 +18,6 @@ Including another URLconf
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/v1/', include('user.urls'))
+    path('api/v1/', include('user.urls')),
+    path('api/v1/', include('team.urls')),
 ]
