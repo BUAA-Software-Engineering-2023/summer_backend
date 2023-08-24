24bba9b7edb341c3b5507dbb3e942a3b7a9947aa
Tony15246 <wunanli2002@163.com>
2023-08-24 17:00:13 +0800

    获取回收站项目
    添加获取回收站项目功能，实现获取被删除项目的API

--- project/urls.py
+++ project/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('project', project_view, name='project'),
-    path('project/<str:id>', get_single_project_view, name='project')
+    path('project', project_view),
+    path('project/deleted', get_deleted_project_view),
+    path('project/<str:id>', get_single_project_view)
 ]
