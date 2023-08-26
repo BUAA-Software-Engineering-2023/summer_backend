43f0ee21893ab87dcf01417bfd9e9a81f1a4ba3e
吴南李 <wunanli2002@163.com>
2023-08-26 23:13:38 +0800

    回滚错误版本
    回滚错误实现同步编辑功能的版本

--- document/models.py
+++ document/models.py
@@ -10,7 +10,7 @@ class Document(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=200)
     content = models.TextField()
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)  # Associate with Project
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
