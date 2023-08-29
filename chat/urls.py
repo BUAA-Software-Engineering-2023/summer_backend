f659a3d0f0ba8063d4aaebee432e33827a88dd49
BienBoy <285915362@qq.com>
2023-08-29 22:50:13 +0800

    新增重命名群组；变更django连接数据库时编码方式为utf8mb4

--- chat/urls.py
+++ chat/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [
     path('chats', ChatListView.as_view()),
     path('chat/<str:pk>', ChatRetrieveView.as_view()),
+    path('chat/<str:pk>/rename', rename_chat_view),
     path('chat/<str:pk>/add-member', add_chat_member_view),
     path('chat/<str:pk>/remove-member', remove_chat_member_view),
     path('chat/<str:pk>/message', get_chat_message_view),
