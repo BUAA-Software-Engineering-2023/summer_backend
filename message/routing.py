187700b5e1d4588821d32ea90e7bfa0adb079022
BienBoy <285915362@qq.com>
2023-08-31 12:20:27 +0800

    完成原型设计预览API

--- message/routing.py
+++ message/routing.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .consumers import MessageConsumer
 
 websocket_urlpatterns = [
-    path('message/<str:user>', MessageConsumer.as_asgi()),
+    path('ws/message/<str:user>', MessageConsumer.as_asgi()),
 ]
