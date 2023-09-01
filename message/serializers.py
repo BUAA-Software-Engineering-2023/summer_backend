434979f5d7cabb46e554b20a2ee119ec98f5ba46
Tony15246 <wunanli2002@163.com>
2023-09-01 12:15:15 +0800

    优化消息获取接口
    优化消息获取接口，给前端传递更多字段

--- message/serializers.py
+++ message/serializers.py
@@ -3,8 +3,20 @@ from .models import Message
 
 
 class MessageSerializer(serializers.ModelSerializer):
+    chat = serializers.SerializerMethodField()
+    team = serializers.SerializerMethodField()
 
     class Meta:
         model = Message
         fields = '__all__'
         extra_kwargs = {"chat_message": {"required": False}, "document": {"required": False}}
+
+    def get_chat(self, message):
+        if message.chat_message:
+            return message.chat_message.chat.id
+        return None
+
+    def get_team(self, message):
+        if message.chat_message:
+            return message.chat_message.chat.team.id
+        return None
