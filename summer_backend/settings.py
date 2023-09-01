bc57ce439b256fed1b300c4a1c86ab96312093cd
吴南李 <wunanli2002@163.com>
2023-09-01 20:24:02 +0800

    纠正误传的数据库配置

--- summer_backend/settings.py
+++ summer_backend/settings.py
@@ -70,28 +70,17 @@ ASGI_APPLICATION = 'summer_backend.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': os.environ.get('DB_NAME', 'summer'),
-#         'USER': os.environ.get('DB_USERNAME', 'root'),
-#         'PASSWORD': os.environ.get('DB_PASSWORD', ''),
-#         'HOST': os.environ.get('DB_HOST', 'localhost'),
-#         'PORT': os.environ.get('DB_PORT', 3306),
-#         'OPTIONS': {
-#             'charset': 'utf8mb4',
-#         }
-#     }
-# }
-
 DATABASES = {
     'default': {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "summer",
-        "USER": "root",
-        "PASSWORD": "Lbj623.623",
-        "HOST": "8.130.50.128",
-        "PORT": 3306
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DB_NAME', 'summer'),
+        'USER': os.environ.get('DB_USERNAME', 'root'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', 3306),
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        }
     }
 }
 
